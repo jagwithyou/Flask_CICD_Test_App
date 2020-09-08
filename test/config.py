eff395a064b660aeccea40f5d6183537d4fcc977
jag6 <jag@sulopa.com>
2020-09-08 16:07:30 +0530

    Initial Commit

---
+++ test/config.py
@@ -0,0 +1,7 @@
+BASE_URL = "http://localhost:8000/"
+DRIVER_PATH = 'drivers'
+
+REPORT_TITLE = "Google Search Testing"
+REPORT_FOLDER = 'reports'
+INDIVIDUAL_REPORT = False
+LOG_FOLDER = 'logs'
---
+++ test/tests/test_flask_page.py
@@ -0,0 +1,20 @@
+
+from selenium.webdriver.support.select import Select
+from page_objects.google_page import GooglePage
+from tests import BaseClass
+from selenium import webdriver
+import pytest, config
+from selenium.common.exceptions import NoSuchElementException
+
+class TestSimplePage(BaseClass):
+    def test_1_dummy(self):
+        ''' This is a dummy test for CICD.'''
+        #Instantiating the logger
+        log = self.getLogger()
+        log.info(" Test Started")
+        #logging the test success
+        log.info("Test Success")
+        assert True
+    
+            
+
